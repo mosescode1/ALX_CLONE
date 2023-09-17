d5d8d7c2b72f0b19fc1c30657ed4e656f4b1b382
Olowosuyi Temitope <temitopeabiodun685@gmail.com>
2023-09-17 23:26:31 +0100

    mobile view first stage

--- src/Dashboard/dashboard.jsx
+++ src/Dashboard/dashboard.jsx
@@ -16,7 +16,7 @@ import { VideoOnDemand } from "./Menu/Video_on_demand";
 import { Peers } from "./Menu/Peers";
 import { CaptainLog } from "./Menu/Captain_log";
 import { Profile } from "./Menu/Profile";
-import { AiFillCalendar } from 'react-icons/ai';
+import { AiFillCalendar, AiOutlineMenu } from 'react-icons/ai';
 import { PiGitBranchBold } from 'react-icons/pi';
 import { GrCheckmark } from 'react-icons/gr';
 import { BsQuestionLg } from 'react-icons/bs';
@@ -90,59 +90,78 @@ export const DashBoard = () =>{
     }
     return(
         <div className="dashFrame">
+            {/* <header className="mobileView">
+                <div className="mobileLogo"></div>
+                <div className="mobileMenuIcon"><AiOutlineMenu/></div>
+            </header> */}
             <div className="leftMenu">
                 <div className="logo" onClick={() => handleMenu('Home_page')}></div>
                 <div className="menuIcons">
                     <div className={currMenu == "My_planning" ? "active" : "icon-container"} onClick={() => handleMenu('My_planning')}>
                         <AiFillCalendar className="icon" />
+                        <p className="mobileIconName">My Planning</p>
                     </div>
                     <div className={currMenu == "Projects" ? "active" : "icon-container"} onClick={() => handleMenu('Projects')}>
                         <PiGitBranchBold className="icon" />
+                        <p className="mobileIconName">Projects</p>
                     </div>
                     <div className={currMenu == "QA" ? "active" : "icon-container"} onClick={() => handleMenu('QA')}>
                         <GrCheckmark className="icon" />
+                        <p className="mobileIconName">QA Reviews I can make</p>
                     </div>
                     <div className={currMenu == "Quiz" ? "active" : "icon-container"} onClick={() => handleMenu('Quiz')}>
                         <BsQuestionLg className="icon" />
+                        <p className="mobileIconName">Evaluation quizzes</p>
                     </div>
                     <hr className="line"/>
                     <div className={currMenu == "Curriculum" ? "active" : "icon-container"} onClick={() => handleMenu('Curriculum')}>
                         <FaGraduationCap className="icon" />
+                        <p className="mobileIconName">Curriculums</p>
                     </div>
                     <div className={currMenu == "Concepts" ? "active" : "icon-container"} onClick={() => handleMenu('Concepts')}>
                         <BsFillFileEarmarkTextFill className="icon" />
+                        <p className="mobileIconName">Concepts</p>
                     </div>
                     <div className={currMenu == "Conference" ? "active" : "icon-container"} onClick={() => handleMenu('Conference')}>
                         <SiWechat className="icon" />
+                        <p className="mobileIconName">Conference rooms</p>
                     </div>
                     <div className={currMenu == "Servers" ? "active" : "icon-container"} onClick={() => handleMenu('Servers')}>
                         <BiSolidServer className="icon" />
+                        <p className="mobileIconName">Servers</p>
                     </div>
                     <div className={currMenu == "Sandboxes" ? "active" : "icon-container"} onClick={() => handleMenu('Sandboxes')}>
                         <LiaGreaterThanEqualSolid className="icon" />
+                        <p className="mobileIconName">Sandboxes</p>
                     </div>
                     <div className={currMenu == "Tools" ? "active" : "icon-container"} onClick={() => handleMenu('Tools')}>
                         <GiSpanner className="icon" />
+                        <p className="mobileIconName">Tools</p>
                     </div>
                     <div className={currMenu == "Video" ? "active" : "icon-container"} onClick={() => handleMenu('Video')}>
                         <BsFilm className="icon" />
+                        <p className="mobileIconName">Video on demand</p>
                     </div>
                     <hr className="line"/>
                     <div className={currMenu == "Peers" ? "active" : "icon-container"} onClick={() => handleMenu('Peers')}>
                         <MdGroups className="icon" />
+                        <p className="mobileIconName">Peers</p>
                     </div>
                     <div className={currMenu == "Captain" ? "active" : "icon-container"} onClick={() => handleMenu('Captain')}>
                         <BiSolidBook className="icon" />
+                        <p className="mobileIconName">Captain's Logs</p>
                     </div><br/><br/>
                 </div>
                 <div className="menuFooter">
                     <div id="icon-container">
                     <a href="https://slack.com" target="_blank">
                         <div className="slack"></div>
+                        <p className="mobileIconName">Slack</p>
                     </a>
                     </div>
                     <div id="icon-container" onClick={() => handleMenu('Profile')}>
                         <div className="profilepic"></div>
+                        <p className="mobileIconName">My Profile</p>
                     </div>
                 </div>
             </div>
